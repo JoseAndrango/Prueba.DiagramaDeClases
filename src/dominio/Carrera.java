710def25b8d92c1beaf530ff077bfef2c962cbfb
jose andrango <joseandrango64@gmail.com>
2021-07-27 12:08:41 -0500

    prueba

---
+++ src/dominio/Carrera.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author usuario
+ */
+public class Carrera {
+    private String ing_civil;
+    private String ing_electrica;
+}
---
+++ src/dominio/Facultad.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author usuario
+ */
+public class Facultad {
+    private Director director;
+    private Carrera carrera ; 
+    private Profesor profesor;
+}
